2d5828840456eb8580c3453ed959ca1376d2b2b0
Tuyet13 <pthituyet298@gmail.com>
2026-04-10 17:02:50 +0700

    Implement mod_inverse function
    Implement modular inverse using extended Euclid's algorithm. Handle case where inverse does not exist.

--- src/mod_inverse.cpp
+++ src/mod_inverse.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+// gcd
 int gcd(int a, int b) {
     while (b != 0) {
         int temp = b;
@@ -11,6 +12,7 @@ int gcd(int a, int b) {
     return a;
 }
 
+// extended euclid
 int extended_euclid(int a, int b, int &x, int &y) {
     if (b == 0) {
         x = 1;
@@ -20,17 +22,25 @@ int extended_euclid(int a, int b, int &x, int &y) {
 
     int x1 = 0, y1 = 0;
     int g = extended_euclid(b, a % b, x1, y1);
+
     x = y1;
     y = x1 - (a / b) * y1;
+
     return g;
 }
 
+// modulo inverse
 int mod_inverse(int a, int m) {
-    // TODO(student): implement modular inverse using extended_euclid()
-    // If inverse does not exist, return -1.
-    (void)a;
-    (void)m;
-    return -1;
+    int x = 0, y = 0;
+    int g = extended_euclid(a, m, x, y);
+
+    // Không tồn tại nghịch đảo
+    if (g != 1) {
+        return -1;
+    }
+
+    // Đảm bảo kết quả dương
+    return (x % m + m) % m;
 }
 
 int main() {
@@ -44,8 +54,14 @@ int main() {
     }
 
     int inv = mod_inverse(a, m);
-    cout << "Nghich dao cua " << a << " mod " << m << " la: " << inv << '\n';
-    cout << "Kiem tra: " << a << " * " << inv << " % " << m
-         << " = " << (1LL * a * inv % m) << '\n';
+
+    if (inv == -1) {
+        cout << "Khong ton tai nghich dao modulo.\n";
+    } else {
+        cout << "Nghich dao cua " << a << " mod " << m << " la: " << inv << '\n';
+        cout << "Kiem tra: " << a << " * " << inv << " % " << m
+             << " = " << (1LL * a * inv % m) << '\n';
+    }
+
     return 0;
 }
