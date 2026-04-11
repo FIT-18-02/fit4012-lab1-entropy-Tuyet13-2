bb8bc9eaea51479d1667be83f4179210b73bbfd3
Tuyet13 <pthituyet298@gmail.com>
2026-04-11 15:55:41 +0700

    Update mod_inverse.cpp

--- src/mod_inverse.cpp
+++ src/mod_inverse.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 
 int gcd(int a, int b) {
-    return (b == 0) ? a : gcd(b, a % b);
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
 }
 
 int extended_euclid(int a, int m, int &x, int &y) {
